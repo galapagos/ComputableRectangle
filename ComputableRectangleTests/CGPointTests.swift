60581b830f2cda1e4124a44739a99867546059e7
kenta.motoyanagi <kenta.motoyanagi@glpgs.com>
2017-05-15 19:19:29 +0900

    assinable CGPoint to right hand.

--- ComputableRectangleTests/CGPointTests.swift
+++ ComputableRectangleTests/CGPointTests.swift
@@ -5,28 +5,52 @@ final class CGPointTests: XCTestCase {
 
     private let basePoint = CGPoint(x: 1, y: 1)
 
-    func test_add() {
+    func test_addToCGFoat() {
         let expect = CGPoint(x: 2, y: 2)
         let actual = basePoint + 1
         XCTAssertEqual(expect, actual)
     }
 
-    func test_sub() {
+    func test_subToCGFoat() {
         let expect = CGPoint.zero
         let actual = basePoint - 1
         XCTAssertEqual(expect, actual)
     }
 
-    func test_mul() {
+    func test_mulToCGFoat() {
         let expect = CGPoint(x: 5, y: 5)
         let actual = basePoint * 5
         XCTAssertEqual(expect, actual)
     }
 
-    func test_div() {
+    func test_divToCGFoat() {
         let expect = CGPoint(x: 0.5, y: 0.5)
         let actual = basePoint / 2
         XCTAssertEqual(expect, actual)
     }
-    
+
+    func test_addToCGPoint() {
+        let expect = CGPoint(x: 2, y: 3)
+        let actual = basePoint + CGPoint(x: 1, y: 2)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_subToCGPoint() {
+        let expect = CGPoint(x: 0, y: -1)
+        let actual = basePoint - CGPoint(x: 1, y: 2)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_mulToCGPoint() {
+        let expect = CGPoint(x: 5, y: 10)
+        let actual = basePoint * CGPoint(x: 5, y: 10)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_divToCGPoint() {
+        let expect = CGPoint(x: 0.5, y: 0.25)
+        let actual = basePoint / CGPoint(x: 2, y: 4)
+        XCTAssertEqual(expect, actual)
+    }
+
 }
--- ComputableRectangleTests/CGRectTests.swift
+++ ComputableRectangleTests/CGRectTests.swift
@@ -5,27 +5,51 @@ final class CGRectTests: XCTestCase {
 
     private let baseRect = CGRect(x: 1, y: 1, width: 1, height: 1)
 
-    func test_add() {
+    func test_addToCGFloat() {
         let expect = CGRect(x: 2, y: 2, width: 2, height: 2)
         let actual = baseRect + 1
         XCTAssertEqual(expect, actual)
     }
 
-    func test_sub() {
+    func test_subToCGFloat() {
         let expect = CGRect.zero
         let actual = baseRect - 1
         XCTAssertEqual(expect, actual)
     }
 
-    func test_mul() {
+    func test_mulToCGFloat() {
         let expect = CGRect(x: 5, y: 5, width: 5, height: 5)
         let actual = baseRect * 5
         XCTAssertEqual(expect, actual)
     }
 
-    func test_div() {
+    func test_divToCGFloat() {
         let expect = CGRect(x: 0.5, y: 0.5, width: 0.5, height: 0.5)
         let actual = baseRect / 2
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_addToCGPoint() {
+        let expect = CGRect(x: 2, y: 3, width: 1, height: 1)
+        let actual = baseRect + CGPoint(x: 1, y: 2)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_subToCGPoint() {
+        let expect = CGRect(x: 0, y: -1, width: 1, height: 1)
+        let actual = baseRect - CGPoint(x: 1, y: 2)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_mulToCGPoint() {
+        let expect = CGRect(x: 5, y: 10, width: 1, height: 1)
+        let actual = baseRect * CGPoint(x: 5, y: 10)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_divToCGPoint() {
+        let expect = CGRect(x: 0.5, y: 0.25, width: 1, height: 1)
+        let actual = baseRect / CGPoint(x: 2, y: 4)
         XCTAssertEqual(expect, actual)
     }
 
