f03926a1fe962918ebdbeced12edc831bebf34c9
kenta.motoyanagi <kenta.motoyanagi@glpgs.com>
2017-05-15 17:22:39 +0900

    Initial Commit

---
+++ ComputableRectangle/ComputableRectangle.h
@@ -0,0 +1,19 @@
+//
+//  ComputableRectangle.h
+//  ComputableRectangle
+//
+//  Created by 本柳賢太 on 2017/05/15.
+//  Copyright © 2017年 k-motoyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ComputableRectangle.
+FOUNDATION_EXPORT double ComputableRectangleVersionNumber;
+
+//! Project version string for ComputableRectangle.
+FOUNDATION_EXPORT const unsigned char ComputableRectangleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComputableRectangle/PublicHeader.h>
+
+
