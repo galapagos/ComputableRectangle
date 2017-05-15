ca12ed266d3a91104e9fc0dc0f35c2bc8d0b4e04
kenta.motoyanagi <kenta.motoyanagi@glpgs.com>
2017-05-15 19:42:53 +0900

    assinable CGSize to right hand.

--- ComputableRectangleTests/CGSizeTests.swift
+++ ComputableRectangleTests/CGSizeTests.swift
@@ -28,5 +28,29 @@ final class CGSizeTests: XCTestCase {
         let actual = baseSize / 2
         XCTAssertEqual(expect, actual)
     }
-    
+
+    func test_addToCGSize() {
+        let expect = CGSize(width: 2, height: 3)
+        let actual = baseSize + CGSize(width: 1, height: 2)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_subToCGSize() {
+        let expect = CGSize(width: 0, height: -1)
+        let actual = baseSize - CGSize(width: 1, height: 2)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_mulToCGSize() {
+        let expect = CGSize(width: 5, height: 10)
+        let actual = baseSize * CGSize(width: 5, height: 10)
+        XCTAssertEqual(expect, actual)
+    }
+
+    func test_divToCGSize() {
+        let expect = CGSize(width: 0.5, height: 0.25)
+        let actual = baseSize / CGSize(width: 2, height: 4)
+        XCTAssertEqual(expect, actual)
+    }
+
 }
