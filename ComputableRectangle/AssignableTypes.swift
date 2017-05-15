f0d87047b35c0706661000522641a96be1de3f02
kenta.motoyanagi <kenta.motoyanagi@glpgs.com>
2017-05-15 20:23:30 +0900

    added assignable types.

---
+++ ComputableRectangle/AssignableTypes.swift
@@ -0,0 +1,73 @@
+import CoreGraphics
+
+public struct OriginX {
+    let value: CGFloat
+    init(_ value: Float) { self.value = CGFloat(value) }
+    init(_ value: Double) { self.value = CGFloat(value) }
+    init(_ value: Float80) { self.value = CGFloat(value) }
+    init(_ value: CGFloat) { self.value = CGFloat(value) }
+    init(_ value: UInt8) { self.value = CGFloat(value) }
+    init(_ value: Int8) { self.value = CGFloat(value) }
+    init(_ value: UInt16) { self.value = CGFloat(value) }
+    init(_ value: Int16) { self.value = CGFloat(value) }
+    init(_ value: UInt32) { self.value = CGFloat(value) }
+    init(_ value: Int32) { self.value = CGFloat(value) }
+    init(_ value: UInt64) { self.value = CGFloat(value) }
+    init(_ value: Int64) { self.value = CGFloat(value) }
+    init(_ value: UInt) { self.value = CGFloat(value) }
+    init(_ value: Int) { self.value = CGFloat(value) }
+}
+
+public struct OriginY {
+    let value: CGFloat
+    init(_ value: Float) { self.value = CGFloat(value) }
+    init(_ value: Double) { self.value = CGFloat(value) }
+    init(_ value: Float80) { self.value = CGFloat(value) }
+    init(_ value: CGFloat) { self.value = CGFloat(value) }
+    init(_ value: UInt8) { self.value = CGFloat(value) }
+    init(_ value: Int8) { self.value = CGFloat(value) }
+    init(_ value: UInt16) { self.value = CGFloat(value) }
+    init(_ value: Int16) { self.value = CGFloat(value) }
+    init(_ value: UInt32) { self.value = CGFloat(value) }
+    init(_ value: Int32) { self.value = CGFloat(value) }
+    init(_ value: UInt64) { self.value = CGFloat(value) }
+    init(_ value: Int64) { self.value = CGFloat(value) }
+    init(_ value: UInt) { self.value = CGFloat(value) }
+    init(_ value: Int) { self.value = CGFloat(value) }
+}
+
+public struct SizeOfWidth {
+    let value: CGFloat
+    init(_ value: Float) { self.value = CGFloat(value) }
+    init(_ value: Double) { self.value = CGFloat(value) }
+    init(_ value: Float80) { self.value = CGFloat(value) }
+    init(_ value: CGFloat) { self.value = CGFloat(value) }
+    init(_ value: UInt8) { self.value = CGFloat(value) }
+    init(_ value: Int8) { self.value = CGFloat(value) }
+    init(_ value: UInt16) { self.value = CGFloat(value) }
+    init(_ value: Int16) { self.value = CGFloat(value) }
+    init(_ value: UInt32) { self.value = CGFloat(value) }
+    init(_ value: Int32) { self.value = CGFloat(value) }
+    init(_ value: UInt64) { self.value = CGFloat(value) }
+    init(_ value: Int64) { self.value = CGFloat(value) }
+    init(_ value: UInt) { self.value = CGFloat(value) }
+    init(_ value: Int) { self.value = CGFloat(value) }
+}
+
+public struct SizeOfHeight {
+    let value: CGFloat
+    init(_ value: Float) { self.value = CGFloat(value) }
+    init(_ value: Double) { self.value = CGFloat(value) }
+    init(_ value: Float80) { self.value = CGFloat(value) }
+    init(_ value: CGFloat) { self.value = CGFloat(value) }
+    init(_ value: UInt8) { self.value = CGFloat(value) }
+    init(_ value: Int8) { self.value = CGFloat(value) }
+    init(_ value: UInt16) { self.value = CGFloat(value) }
+    init(_ value: Int16) { self.value = CGFloat(value) }
+    init(_ value: UInt32) { self.value = CGFloat(value) }
+    init(_ value: Int32) { self.value = CGFloat(value) }
+    init(_ value: UInt64) { self.value = CGFloat(value) }
+    init(_ value: Int64) { self.value = CGFloat(value) }
+    init(_ value: UInt) { self.value = CGFloat(value) }
+    init(_ value: Int) { self.value = CGFloat(value) }
+}
